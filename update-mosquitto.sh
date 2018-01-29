3bd7d310c5089789bc3d94f69e3b7b1c1a38a575
bibarrav <bibarrav@hotmail.com>
2018-01-28 22:29:01 -0300

    Updated to use a custom build image using latest eclipse mosquitto version

--- update-mosquitto.sh
+++ update-mosquitto.sh
@@ -1,11 +1,14 @@
 #Update mosquitto Docker image and then re-create the container 
+docker pull bibarrav/mosquitto-docker #eclipse-mosquitto
 docker stop mosquitto                                                                                    
 docker rm mosquitto
 
-docker run --name=mosquitto \ 
--p 1883:1883 -p 9001:9001 \ 
--v /opt/mosquitto/config:/mosquitto/config/ \ 
--v /opt/mosquitto/data:/mosquitto/data \ 
--v /opt/mosquitto/log:/mosquitto/log \ 
---restart=always  -d \ 
-eclipse-mosquitto
+docker run --name=mosquitto \
+-p 1883:1883 -p 9001:9001 \
+-v /opt/mosquitto/config:/mosquitto/config \
+-v /opt/mosquitto/data:/mosquitto/data \
+-v /opt/mosquitto/log:/mosquitto/log \
+--restart=always  -d \
+bibarrav/mosquitto-docker
+
+#eclipse-mosquitto
